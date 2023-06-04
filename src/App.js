4c243a463d14aff7ac2627b0e730f787238b90a9
URSULA BABETO <ursulacobabeto@gmail.com>
2023-06-04 12:48:31 -0300

    COMMITin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          HELLO WORLD!!!
+          HELLO WORLD!!!!
         </p>
       </header>
     </div>
